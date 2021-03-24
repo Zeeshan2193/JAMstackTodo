3509db40faaf36aa07cab3d9a240d8a488caf0ab
Zeeshan Ali <zeeshan2193@gmail.com>
2021-03-24 19:21:13 +0500

    Color Done
    Color Done

---
+++ packages/www/wrap-root-element.js
@@ -0,0 +1,7 @@
+// basic usage
+const React = require('react');
+const { ThemeProvider } =require ('theme-ui');
+const {light} = require ( '@theme-ui/presets' );
+module.exports = ({element}) => (
+  <ThemeProvider theme={light}>{element}</ThemeProvider>
+)
