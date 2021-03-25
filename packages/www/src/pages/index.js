86af7225822eacc1b97f6170c0466a6a647323f5
Zeeshan Ali <zeeshan2193@gmail.com>
2021-03-25 23:22:54 +0500

    Netlify Identity
    Netlify Identity integration is done in this step

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import {Container, Heading, Button, Flex} from 'theme-ui';
+import netlifyIdentity from 'netlify-identity-widget';
 
-export default props => (
-        <div>
-            Hello Zeeshan Ali Rajpoot
-        </div>
-    );
+export default props => {
+    useEffect(() => {
+        netlifyIdentity.init({});
+         
+    }, [])
+    return(
+        <Container>
+            <Flex sx={{flexDirection :'column', padding : 3}}>
+            <Heading as="h1">
+            TODO APP
+            </Heading>
+            <Button sx={{marginTop : 2, color:'black'}} onClick={()=>{netlifyIdentity.open()}}>Log In </Button>
+            </Flex>
+        </Container>
+    )};
 
--- packages/www/wrap-root-element.js
+++ packages/www/wrap-root-element.js
@@ -2,6 +2,10 @@
 const React = require('react');
 const { ThemeProvider } =require ('theme-ui');
 const {light} = require ( '@theme-ui/presets' );
+const newTheme= {
+  ...light,
+  sizes: {container: 1024}
+};
 module.exports = ({element}) => (
-  <ThemeProvider theme={light}>{element}</ThemeProvider>
+  <ThemeProvider theme={newTheme}>{element}</ThemeProvider>
 )
